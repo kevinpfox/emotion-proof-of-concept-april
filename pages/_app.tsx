357c1a708bd4fa25ca4268f7d9d8ecf317a63fd9
Kevin Fox <57231794+kevinpfox@users.noreply.github.com>
2023-04-29 10:22:23 -0700

    Add StyledEngineProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
@@ -25,7 +26,9 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <StyledEngineProvider injectFirst>
+          <Component {...pageProps} />
+        </StyledEngineProvider>
       </ThemeProvider>
     </CacheProvider>
   );
